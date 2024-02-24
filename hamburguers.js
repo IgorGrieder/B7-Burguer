e12f196d9dced2a8e3d757b54716d9d5c5044295
Igor Grieder Melo <igorgrieder@gmail.com>
2024-02-24 15:08:29 -0300

    Preenchimento do banco de dados de hamburguer

--- hamburguers.js
+++ hamburguers.js
@@ -1,5 +1,50 @@
 let hamburguers = [
     { 
-        name: ""
-    }
+        name: "Old Skool",
+        category: "Artesanal",
+        desc: "Aquele clássico que todo mundo adora: Pão de batata selado na manteiga, burger artesanal, queijo cheddar em fatias e derretido, ketchup rústico da casa, cebola, picles e mostarda.",
+        price: [25.99, 20.99],
+        promoPrice: true,
+        img: "imgs/burger1.png"
+    },
+    { 
+        name: "X-Burger",
+        category: "Podrão",
+        desc: "Pão fofinho, carne de 90g com aquela mussarela derretida por cima, alface, tomate fresco, milho, batata palha crocante e molho rosé da casa.",
+        price: [18.90, 15.60],
+        promoPrice: false,
+        img: "imgs/burger2.png"
+    },
+    { 
+        name: "Black Belt",
+        category: "Artesanal",
+        desc: "É só o campeão de pedidos: Pão tipo australiano selado na manteiga, burger artesanal, muuuito cheddar em fatias derretido por cima dos bacons carnudos e cebola caramelizada ao molho teriyaki.",
+        price: [36.90, 29.90],
+        promoPrice: false,
+        img: "imgs/burger3.png"
+    },
+    { 
+        name: "X-Bacon",
+        category: "Podrão",
+        desc: "Pão fofinho, carne de 90g com aquela mussarela derretida por cima, presunto, bacon em cubos, alface, tomate fresco, milho, batata palha crocante e molho rosé da casa.",
+        price: [22.90, 18.49],
+        promoPrice: false,
+        img: "imgs/burger4.png"
+    },
+    { 
+        name: "Trucker",
+        category: "Artesanal",
+        desc: "Já tiramos do cardápio um vez e deu briga: Pão brioche selado na manteiga, burger artesanal, queijo cheddar derretido, ovo frito, tomate, geléia agridoce de bacon em cubos, preparada diariamente em nossa cozinha.",
+        price: [28.90, 23.99],
+        promoPrice: false,
+        img: "imgs/burger1.png"
+    },
+    { 
+        name: "X-Egg Bacon",
+        category: "Podrão",
+        desc: "Pão fofinho, carne 90g com aquela mussarela derretida por cima, presunto, bacon em cubos, ovo estalando, alface, tomate fresco, milho, batata palha e molho rosé da casa.",
+        price: [24.99, 20.99],
+        promoPrice: false,
+        img: "imgs/burger2.png"
+    },
 ]
--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const links = document.querySelectorAll("a");
 
 // ----------------------------- events -----------------------------
 
+
 // avoid links to reopen the page
 links.forEach((item) => item.addEventListener("click", (event) => event.preventDefault()));
